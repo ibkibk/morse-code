ee1fec68b76c79c6bc92fbba2d93365b303c2b33
Ibrahim Kurhan <Ibrahim.Kurhan@Nology.local>
2020-03-25 20:17:43 +0000

    latest version

---
+++ app.js
@@ -0,0 +1,80 @@
+const type = document.getElementsByClassName("typeinput");
+
+const letters =
+{
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '0': '-----',
+    '.': '.-.-.-',
+    ',': '--..--',
+    '?': '..--..',
+    "'": '.----.',
+    '!': '-.-.--',
+    '/': '-..-.',
+    '(': '-.--.',
+    ')': '-.--.-',
+    '&': '.-...',
+    ':': '---...',
+    ';': '-.-.-.',
+    '=': '-...-',
+    '+': '.-.-.',
+    '-': '-....-',
+    '_': '..--.-',
+    '"': '.-..-.',
+    '$': '...-..-',
+    '@': '.--.-.',
+    ' ': ' '
+}
+const convert = (letter) => Object.values(letters).find((value) => value === letters[letter]);
+// const word = "ibrahim";
+// const arrayWord = word.split(""); 
+// const arrayMorse = arrayWord.map((letter) => convert(letter));
+// const morse = arrayMorse.join("");
+
+const mapToMorseCode = () => {
+   const userInput = type[0].value;
+   const letterArray= userInput.split("");
+   const convertedLetterArray = letterArray.map(convert);
+   const resulted = convertedLetterArray.join("");
+   document.getElementById("result").innerHTML= resulted;
+}
+document.getElementById("convert").addEventListener("click",mapToMorseCode );
+
+
+
+
+
+
